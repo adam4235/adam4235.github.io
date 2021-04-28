44ca94e47cf4eb2cceb08c4e299a1a3eaaea7dde
Adam Richard <devnull@localhost>
2021-04-28 12:53:53 -0300

    Try to work around assertion failure when touching with 2 fingers on Kindle Fire tablet with Firefox

--- algebra/algebra.js
+++ algebra/algebra.js
@@ -8535,7 +8535,7 @@ function _emscripten_asm_const_iii(code, sigPtr, argbuf) {
         for(var i = 0; i < et.length; ++i) {
           var touch = et[i];
           // Verify that browser does not recycle touch objects with old stale data, but reports new ones each time.
-          assert(!touch.isChanged);
+          //assert(!touch.isChanged);
           assert(!touch.onTarget);
           touches[touch.identifier] = touch;
         }
