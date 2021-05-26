23736b430d7deea75f7e1b6d2c8c43657cfda83a
Adam Richard <devnull@localhost>
2021-05-26 14:50:15 -0300

    Updated algebra game to fix the size of the arrow in message boxes

--- algebra/algebra.js
+++ algebra/algebra.js
@@ -640,8 +640,8 @@ var wasmMemory;
 // In the wasm backend, we polyfill the WebAssembly object,
 // so this creates a (non-native-wasm) table for us.
 var wasmTable = new WebAssembly.Table({
-  'initial': 2107,
-  'maximum': 2107 + 0,
+  'initial': 2106,
+  'maximum': 2106 + 0,
   'element': 'anyfunc'
 });
 
